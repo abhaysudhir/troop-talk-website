d09b3d6ae314b61538f1b749b046df1789d010e6
abhays72 <abhaysudhir@icloud.com>
2025-01-02 00:05:04 -0800

    added digital ocean python app url to chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -33,7 +33,7 @@ const Chat = () => {
 
     try {
       const response = await fetch(
-        `https://64.23.142.139/ask?question=${encodeURIComponent(input)}&top_k=5`,
+        `https://tt-backend-scripts-bigvm.ondigitalocean.app/ask?question=${encodeURIComponent(input)}&top_k=5`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
