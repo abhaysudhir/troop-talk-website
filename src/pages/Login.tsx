00d78749e570e8464812915d7bf59d7ebeee8060
abhays72 <abhaysudhir@icloud.com>
2025-01-15 16:39:43 -0800

    cleaning up with prettier formatting + minor changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,30 @@
-import { SignIn } from '@clerk/clerk-react';
-import { Helmet } from 'react-helmet-async';
+import { SignIn } from "@clerk/clerk-react";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Login | Troop Talk</title>
-      </Helmet>
-      <div className="min-h-screen flex items-center justify-center">
-        <SignIn 
-          routing="path"
-          path="/sign-in"
-          signUpUrl="/sign-up"
-          afterSignInUrl="/chat"
-          appearance={{
-            layout: {
-              socialButtonsPlacement: "bottom",
-              socialButtonsVariant: "iconButton",
-              termsPageUrl: "https://clerk.com/terms"
-            }
-          }}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Login | Troop Talk</title>
+            </Helmet>
+            <div className="min-h-screen flex items-center justify-center">
+                <SignIn
+                    routing="path"
+                    path="/sign-in"
+                    signUpUrl="/sign-up"
+                    afterSignInUrl="/chat"
+                    appearance={{
+                        layout: {
+                            socialButtonsPlacement: "top",
+                            socialButtonsVariant: "iconButton",
+                            termsPageUrl:
+                                "https://trooptalk.ai/public/terms-and-conditions.html",
+                        },
+                    }}
+                />
+            </div>
+        </>
+    );
 };
 
 export default Login;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,29 +1,30 @@
-import { SignUp } from '@clerk/clerk-react';
-import { Helmet } from 'react-helmet-async';
+import { SignUp } from "@clerk/clerk-react";
+import { Helmet } from "react-helmet-async";
 
 const Signup = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Sign Up | Troop Talk</title>
-      </Helmet>
-      <div className="min-h-screen flex items-center justify-center">
-        <SignUp 
-          routing="path"
-          path="/sign-up"
-          signInUrl="/sign-in"
-          afterSignUpUrl="/chat"
-          appearance={{
-            layout: {
-              socialButtonsPlacement: "bottom",
-              socialButtonsVariant: "iconButton",
-              termsPageUrl: "https://clerk.com/terms"
-            }
-          }}
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Sign Up | Troop Talk</title>
+            </Helmet>
+            <div className="min-h-screen flex items-center justify-center">
+                <SignUp
+                    routing="path"
+                    path="/sign-up"
+                    signInUrl="/sign-in"
+                    afterSignUpUrl="/chat"
+                    appearance={{
+                        layout: {
+                            socialButtonsPlacement: "top",
+                            socialButtonsVariant: "iconButton",
+                            termsPageUrl:
+                                "https://trooptalk.ai/terms-and-conditions.html",
+                        },
+                    }}
+                />
+            </div>
+        </>
+    );
 };
 
 export default Signup;
