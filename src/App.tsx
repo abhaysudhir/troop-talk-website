17ca63268d38e0a72920e1d22e21683f8567addc
abhays72 <abhaysudhir@icloud.com>
2025-01-17 11:17:18 -0800

    working routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Chat from './pages/Chat';
 import PendingApproval from './pages/PendingApproval';
 import AdminDashboard from './pages/AdminDashboard';
 import Landing from './pages/Landing';
-import Landing from './pages/Landing';
 import { HelmetProvider } from 'react-helmet-async';
 
 export default function App() {
@@ -49,19 +48,20 @@ export default function App() {
                         }
                     />
 
-          {/* <Route
-            path="/chat"
-            element={
-              <>
-                <SignedIn>
-                  <Chat />
-                </SignedIn>
-                <SignedOut>
-                  <RedirectToSignIn />
-                </SignedOut>
-              </>
-            }
-          /> */}
+                    <Route
+                        path="/chat"
+                        element={
+                            <>
+                                <SignedIn>
+                                    <Chat />
+                                </SignedIn>
+                                <SignedOut>
+                                    <RedirectToSignIn />
+                                </SignedOut>
+                            </>
+                        }
+                    />
+                    */}
 
                     {/* Catch all route - redirect everything to root */}
                     <Route path="/*" element={<Navigate to="/" replace />} />
